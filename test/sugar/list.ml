034b4eff556dc09cb9f858fdb4c51e4760e1e597
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-03-29 16:35:53 +0200

    improve example

--- test/sugar/list.ml
+++ test/sugar/list.ml
@@ -9,3 +9,13 @@ let y = snd (a)
 
 
 let nl = ("nuovoelemento", a)
+
+let is_empty l = match fst (l) with () -> true | _ -> false
+
+
+let rec len = fun x -> match fst (x) with () -> 0 | _ -> 1 + len (snd (x))
+
+let rec map = fun (f, x) -> 
+  match x with 
+  | () -> () 
+  | _ -> (hd (x) |> f, tl (x) |> map (f))
