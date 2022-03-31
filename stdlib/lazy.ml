d0d5ffab1d147339540cebff4f8a4f9bb4dfd48c
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-03-31 19:15:19 +0200

    lazy mod

--- stdlib/lazy.ml
+++ stdlib/lazy.ml
@@ -3,5 +3,7 @@ module Lazy = struct
 
   let force = fun x -> x ()
 
-  let lazy = fun f -> fun () -> f ()
+  let from_fun = fun f: (unit -> 'a) -> f
+  let from_val = fun x -> lazy (fun () -> x)
+  let map = fun f -> fun x -> f (force (x))
 end
